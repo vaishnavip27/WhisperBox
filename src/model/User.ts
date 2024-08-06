b8cf0dc14bb43aded4884955c264f1a03ef189bd
Vaishnavi Patil <vaishnavip276@gmail.com>
2024-08-06 22:31:06 +0530

    commit

---
+++ src/model/User.ts
@@ -0,0 +1,18 @@
+import { Message } from "./User";
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface Message extends Document {
+  content: "string";
+  createdAt: Date;
+}
+
+const MessageSchema: Schema<Message> = new Schema({
+  content: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+  },
+});
