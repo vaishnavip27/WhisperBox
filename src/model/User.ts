77af47548f8aade53bfd907cee0b1a5dce0cb873
Vaishnavi Patil <vaishnavip276@gmail.com>
2024-08-07 16:54:56 +0530

    commit

--- src/model/User.ts
+++ src/model/User.ts
@@ -14,5 +14,61 @@ const MessageSchema: Schema<Message> = new Schema({
   createdAt: {
     type: Date,
     required: true,
+    default: Date.now,
   },
+});
+
+export interface User extends Document {
+  username: string;
+  email: string;
+  password: string;
+  verifyCode: string;
+  verifyCodeExpiry: string;
+  isVerified: boolean;
+  isAcceptingMessage: boolean;
+  messages: Message[];
+}
+
+const UserSchema: Schema<User> = new Schema({
+  username: {
+    type: String,
+    required: [true, "Username is required"],
+    trim: true,
+    unique: true,
+  },
+
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+    match: [/.+\@.+\..+/, "Please use a valid email address"],
+    default: Date.now,
+  },
+
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+  },
+
+  verifyCode: {
+    type: String,
+    required: [true, "Verification code is required"],
+  },
+
+  verifyCodeExpiry: {
+    type: Date,
+    required: [true, "Verify code expiry is required"],
+  },
+
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+
+  isAcceptingMessage: {
+    type: Boolean,
+    default: true,
+  },
+
+  messages: [MessageSchema],
 });
